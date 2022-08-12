0763a62807852479b6f6e8a21ef195ea062ef0c3
jryjng <shadowspirit56@gmail.com>
2022-08-11 19:16:02 -0500

    init

---
+++ main.js
@@ -0,0 +1,60 @@
+// From dat.js contains data (array)
+
+// Lunch ends at 2pm?
+const DINNER = 14;
+
+let now = new Date();
+
+// Test
+console.log(now);
+
+// Time priority
+let isDinner = now.getHours() > DINNER;
+let timeValue = now.getDay() * 2 + (isDinner ? 1 : 0); 
+
+// TMP TESTING
+timeValue = 0;
+
+// Set the date
+document.querySelector("#date").textContent = data[0].week;
+
+// Loop through the rows
+let menus = document.querySelectorAll(".row-menu");
+let parent = menus[0].parentNode;
+for(let timeIdx = 0; timeIdx < menus.length; timeIdx++){
+    let currentElem = menus[timeIdx];
+
+    // Remove outdated schedules
+    if (timeValue > timeIdx){
+        parent.removeChild(currentElem);
+        continue;
+    }
+
+    // Update the current row
+    let serveries = currentElem.children;
+    for(let serveryIdx = 1; serveryIdx < serveries.length; serveryIdx++){
+        // Clear original text content
+        serveries[serveryIdx].textContent = "";
+
+        // Get the menu for the servery at the specified time
+        // [servery][mealtime][day][menu][food][type/food]
+        let menu = null;
+        if (isDinner){
+            menu = data[serveryIdx - 1]["dinner"][Math.floor(timeIdx / 2)];
+        }else{
+            menu = data[serveryIdx - 1]["lunch"][Math.floor(timeIdx / 2)];
+        }
+
+        // Create an element from each menu
+        // The first entry will contain text. Everything else is part of its class
+        for(let foodIdx = 0; foodIdx < menu.length; foodIdx++){
+            let elem = document.createElement("p");
+            let food = menu[foodIdx];
+            elem.textContent = food[0];
+            for(let classIdx = 1; classIdx < food.length; classIdx++){
+                elem.classList.add(food[classIdx]);
+            }
+            serveries[serveryIdx].appendChild(elem);
+        }
+    }
+}
